bc087e3a00d77b643b8261f977cd52805fb89a52
FARBEX97 <64985555+FARBEX97@users.noreply.github.com>
2020-09-17 20:18:23 +0200

    añadida buscarx_celda

--- asistentes/asistente_excel.py
+++ asistentes/asistente_excel.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 
-def buscarv(df_resultado, valor_comun, valor_buscado_izq, valor_buscado_dcha, df_busqueda):
+def buscarv_col(df_resultado, valor_comun, valor_buscado_izq, valor_buscado_dcha, df_busqueda):
     """
     Ejecuta una función igual a BuscarV en Excel. Devuelve pd.DataFrame()
     df_resultado = DF sobre el que deseamos efectuar el BuscarV.
@@ -15,4 +15,14 @@ def buscarv(df_resultado, valor_comun, valor_buscado_izq, valor_buscado_dcha, df
     df_aux = df_resultado.merge(df_busqueda, on=valor_comun, how='left')
     df_resultado[valor_buscado_izq] = df_aux[valor_buscado_dcha]
     return df_resultado
+
+
+def buscarx_celda(valor_buscado, columna_buscar, columna_devolver):
+    """
+    Fuente: pythoninoffice.com
+    valor_buscado: valor en el que estamos interesados, será una string.
+    columna_buscar: columna en la que vamos a buscar el valor buscado.
+    columna_devolver: columna en la que esperamos encontrar un valor.    
+    """
+    return columna_devolver.loc[columna_buscar == valor_buscado].tolist()[0]
 
