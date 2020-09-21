fdbbb99e09ec7205b1d0ffbf749f6b80914e92ec
Fernando Arbex <64985555+FARBEX97@users.noreply.github.com>
2020-09-21 20:20:19 +0200

    Update crear_virtualenv.bat

--- scripts/crear_virtualenv.bat
+++ scripts/crear_virtualenv.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
-:comentario
+:Crea el entorno virtual utilizando virtualenv
+:Para instalar virtualenv utiliza el siguiente comando en el terminal
+: "pip install virtualenv"
 virtualenv venv
 pip install --upgrade pip
 pip install -r requirements.txt
