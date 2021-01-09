4451996997daf7824ab2b98681ba9e2a218f4dee
FARBEX97 <64985555+FARBEX97@users.noreply.github.com>
2021-01-09 18:46:47 +0100

    script eliminado

--- scripts/crear_virtualenv.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-:Crea el entorno virtual utilizando virtualenv
-:Para instalar virtualenv utiliza el siguiente comando en el terminal
-: "pip install virtualenv"
-virtualenv venv
-pip install --upgrade pip
-pip install -r requirements.txt
-PAUSE
--- scripts/ejecutar_script.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-:Donde pone venv, pon el nombre de tu entorno virtual
-"%~dp0venv\Scripts\python.exe" "script.py"
-PAUSE
