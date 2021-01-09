9be8109982e56c61fccf86a11c3eb1eeeac88e15
FARBEX97 <64985555+FARBEX97@users.noreply.github.com>
2021-01-10 00:00:48 +0100

    añadido main()

--- scripts/encontrar_duplicados.py
+++ scripts/encontrar_duplicados.py
@@ -2,30 +2,35 @@ from coffeece.SysWalker import SysWalker as sw
 from coffeece.Popup import Popup as pu
 
 
-# Crea un diccionario con las rutas, carpetas y archivos
-# dentro de la carpeta especificada por el usuario
-src_directory = pu.ask_directory()
-files_tree = sw.list_all_files_recursively(src_directory)
+def main():
+    # Crea un diccionario con las rutas, carpetas y archivos
+    # dentro de la carpeta especificada por el usuario
+    src_directory = pu.ask_directory()
+    files_tree = sw.list_all_files_recursively(src_directory)
 
-# Extrae los nombres de archivo duplicados y crea un diccionario
-# relacionando nombres de archivo duplicados y las rutas que tienen
+    # Extrae los nombres de archivo duplicados y crea un diccionario
+    # relacionando nombres de archivo duplicados y las rutas que tienen
 
-unique_filenames = []
-duplicate_files = {}
+    unique_filenames = []
+    duplicate_files = {}
 
-for root in files_tree:
-    for file in files_tree[root]["files"]:
-        if file in unique_filenames:
-            if file in duplicate_files:
-                duplicate_files[file]["paths"].append(root)
+    for root in files_tree:
+        for file in files_tree[root]["files"]:
+            if file in unique_filenames:
+                if file in duplicate_files:
+                    duplicate_files[file]["paths"].append(root)
+                else:
+                    duplicate_files[file] = {}
+                    duplicate_files[file]["paths"] = []
+                    duplicate_files[file]["paths"].append(root)
             else:
-                duplicate_files[file] = {}
-                duplicate_files[file]["paths"] = []
-                duplicate_files[file]["paths"].append(root)
-        else:
-            unique_filenames.append(file)
+                unique_filenames.append(file)
 
-for file in duplicate_files:
-    print(f"File {file} exists on various locations:")
-    for path in duplicate_files[file]["paths"]:
-        print(path)
+    for file in duplicate_files:
+        print(f"File {file} exists on various locations:")
+        for path in duplicate_files[file]["paths"]:
+            print(path)
+
+
+if __name__ == '__main__':
+    main()
